1145e1a0126f17b71a94424a41f48a4b6912b5be
Kevin Sims <kevinsims@Kevins-MacBook-Pro.local>
2019-06-25 13:50:33 -0700

    private route working

--- donormanagement/src/App.js
+++ donormanagement/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Route } from "react-router-dom";
 
+import ProtectedRoute from "./components/ProtectedRoute"
+
 import SignupPage from './components/SignUpPage'
 import DonationsPage from './components/DonationsPage'
 
-// import PrivateRoute from "./components/PrivateRoute";
+
 
 import "./App.css";
 import AddDonorPage from "./components/AddDonorPage";
@@ -12,7 +14,7 @@ import AddDonorPage from "./components/AddDonorPage";
 function App() {
   return (
     <div className="App">
-      <Route path="/donationform" component={AddDonorPage} />
+      <ProtectedRoute path="/donationform" component={AddDonorPage} />
       <Route path="/signup" component={SignupPage} />
       <Route exact path="/" component={DonationsPage} />  
       {/* <PrivateRoute path="/" component={} />  */}
