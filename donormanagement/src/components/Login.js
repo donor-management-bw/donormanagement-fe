1c5649a6c31177986ceedded8ecb98d1867d9776
Kevin Sims <kevinsims@Kevins-MacBook-Pro.local>
2019-06-26 10:57:27 -0700

    donors showing

--- donormanagement/src/components/Login.js
+++ donormanagement/src/components/Login.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { login } from '../actions/index';
-import Loader from 'react-loader-spinner';
+import Loader from 'react-loader-spinner'
 
 import './Login.css';
 
